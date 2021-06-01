63cf5690604062dfdbd954a3c4993fb4c1b2602a
ranga608667 <rangarajan.murugan@cognizant.com>
2021-06-01 14:43:56 -0500

    first commit

---
+++ src/main/java/com/example/crudApiCheckpoints/CrudApiCheckpointsApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudApiCheckpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApiCheckpointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApiCheckpointsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crudApiCheckpoints/CrudApiCheckpointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudApiCheckpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiCheckpointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
